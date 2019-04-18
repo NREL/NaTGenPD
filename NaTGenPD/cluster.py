65d3cf6292e32033e3bb27da0c76e0f3df73f32a
Michael Rossol <michael.rossol@nrel.gov>
2019-04-18 14:05:32 -0600

    SingleCluster class added

--- NaTGenPD/cluster.py
+++ NaTGenPD/cluster.py
@@ -15,137 +15,6 @@ from sklearn.preprocessing import normalize
 logger = logging.getLogger(__name__)
 
 
-def single_cluster(df, cols=None, NN=5, dist=0.1, normalize=True):
-    """
-    Use euclidian KNN to reduce noisy dataset to one cluster.
-
-    Parameters
-    ----------
-    df : pd.DataFrame
-        DataFrame to cluster.
-    cols : list | NoneType
-        Columns in df to cluster on. None clusters on all columns in df.
-    NN : int
-        Number of desired nearest neighbors to be considered in the central
-        cluster.
-    dist : int or float
-        Euclidian distance between coordinates to be considered a neighbor.
-    normalize : bool
-        Option to perform range-normalization on data before clustering.
-
-    Returns
-    -------
-    df : pd.DataFrame
-        Subset of input df with outlier data removed.
-    """
-
-    n_dat = len(df)
-    if n_dat <= 1:
-        # coordinate set with only one coordinate cannot be cleaned further
-        return df
-
-    NN = np.min([NN, int(n_dat / 3) + 1])
-    N = int(np.ceil(NN)) + 2
-    if N > n_dat:
-        raise ValueError('Number of nearest neighbors to retrieve (N={}) '
-                         'exceeds length of the dataframe {}'.format(N, n_dat))
-    logger.debug('Set contains {} coordinates, N set to {}, '
-                 'N-neighbors required: {} at {}'
-                 .format(n_dat, N, NN, dist))
-
-    # get nearest neighbors
-    d, _ = knn(df, cols, return_dist=True, normalize=normalize, k=N)
-
-    # count the number of distance results per index less than the threshold
-    # (exclude the 1st dist row which is self-referential)
-    counts = np.sum(d[:, 1:] < dist, axis=1)
-
-    # find where there are NN neighbors satisfying the distance threshold
-    mask = np.where(counts > NN)[0]
-
-    logger.debug('{} points after cleaning'.format(len(mask)))
-
-    if len(mask) > 1:
-        # multiple cleaned points exist, return them
-        return df.iloc[mask, :]
-    else:
-        # Cleanup eliminated all points. Retry with less strict thresholds
-        return single_cluster(df, cols=cols, NN=int(NN * 0.8), dist=dist * 1.2,
-                              normalize=normalize)
-
-
-def knn(df, cols, return_dist=False, normalize=True, k=1):
-    """Get euclidian distance nearest neighbors for numerical column data.
-
-    Parameters
-    ----------
-    df : pd.DataFrame
-        DataFrame with data to cluster.
-    cols : list | NoneType
-        Columns in df to cluster on. None clusters on all columns in df.
-    return_dist : bool
-        Determines whether this method returns (dist, ind) or just ind
-    normalize : str
-        Determines whether the data is range-normalized by column.
-    k : int
-        The number of k-th nearest neighbors to return.
-
-    Returns
-    -------
-    dist : np.ndarray
-        Each entry gives the list of distances to the neighbors of the
-        corresponding point.
-    ind : np.ndarray
-        Each entry gives the list of indices of neighbors of the corresponding
-        point.
-    """
-
-    # take slices of df1/df2 based on col slices
-    if cols is None:
-        cols = df.columns.values
-
-    array1 = df.loc[:, cols].values
-    array2 = df.loc[:, cols].values
-
-    try:
-        # make sure these columns can be cast as float (required for KNN)
-        array1 = array1.astype(dtype=float)
-        array2 = array2.astype(dtype=float)
-    except Exception as e:
-        raise e
-
-    if normalize:
-        for i, _ in enumerate(cols):
-            # get min and max values from a concatenation of both arrays
-            min_all = np.nanmin((np.nanmin(array1[:, i]),
-                                 np.nanmin(array2[:, i])))
-            max_all = np.nanmax((np.nanmax(array1[:, i]),
-                                 np.nanmax(array2[:, i])))
-            range_all = max_all - min_all
-
-            if range_all == 0.0:
-                # protect against div by zero
-                range_all = 1
-
-            # range scale from 0 to 1
-            array1[:, i] -= min_all
-            array1[:, i] /= range_all
-            array2[:, i] -= min_all
-            array2[:, i] /= range_all
-
-    # execute KNN query
-    tree = cKDTree(array2)
-    dist_1, ind_1 = tree.query(array1, k=k)
-
-    logger.debug('KNN index nearest neighbors: \n{}'.format(ind_1))
-    logger.debug('KNN distance: \n{}'.format(dist_1))
-
-    if return_dist:
-        return dist_1, ind_1
-    else:
-        return ind_1
-
-
 class Cluster:
     """
     DBSCAN clustering class
@@ -169,7 +38,7 @@ class Cluster:
         return '{} for {} unit {}'.format(name, self._type, self._unit_id)
 
     @staticmethod
-    def normalize_values(arr, norm='max'):
+    def normalize_values(arr, norm=None):
         """
         Normalize values in array by column
 
@@ -187,7 +56,18 @@ class Cluster:
             array with values normalized by column
             shape (n samples, with m features)
         """
-        return normalize(arr, axis=0, norm=norm)
+        if norm:
+            arr = normalize(arr, axis=0, norm=norm)
+        else:
+            min_all = arr.min(axis=0)
+            max_all = arr.max(axis=0)
+            range_all = max_all - min_all
+            pos = range_all == 0
+            range_all[pos] = 1
+            arr -= min_all
+            arr /= range_all
+
+        return arr
 
     @staticmethod
     def n_dist(array, n):
@@ -313,7 +193,7 @@ class Cluster:
         eps = k_dist[eps_pos]
         return eps
 
-    def _DBSCAN(self, array, min_samples, eps=None):
+    def _cluster(self, array, min_samples, eps=None):
         """
         Run DBSCAN on array, compute eps if not supplied.
 
@@ -372,7 +252,7 @@ class Cluster:
             min_samples value used to run DBSCAN
         """
         outliers = kwargs.get('outliers', False)
-        labels, eps, _ = self._DBSCAN(array, min_samples)
+        labels, eps, _ = self._cluster(array, min_samples)
         label_n = [_l for _l in np.unique(labels) if _l != -1]
         n_clusters = len(label_n)
         eps_dt = eps * dt
@@ -382,7 +262,7 @@ class Cluster:
         while len(label_n) > 1:
             eps += eps_dt
             eps_dt = eps / dt
-            labels, _, _ = self._DBSCAN(array, min_samples, eps=eps)
+            labels, _, _ = self._cluster(array, min_samples, eps=eps)
 
             label_n = [_l for _l in np.unique(labels) if _l != -1]
             if len(label_n) == n_clusters:
@@ -429,7 +309,7 @@ class Cluster:
         cluster_params = None
         score = None
         while True:
-            labels, eps, _ = self._DBSCAN(array, min_samples)
+            labels, eps, _ = self._cluster(array, min_samples)
 
             label_n = [_l for _l in np.unique(labels) if _l != -1]
             if len(label_n) == n_clusters:
@@ -465,3 +345,94 @@ class SingleCluster(Cluster):
     """
     Subclass to perform single cluster extraction on non-CC generators
     """
+    def __init__(self, unit_df):
+        super().__init__(unit_df)
+        self._tree = cKDTree(self.get_data(['load', 'heat_rate']))
+
+    @staticmethod
+    def knn(arr, tree=None, k=1):
+        """Get euclidian distance nearest neighbors for numerical column data.
+
+        Parameters
+        ----------
+        array : ndarray
+            Array to be used for clustering, shape n samples with m features
+        k : int
+            The number of k-th nearest neighbors to return.
+
+        Returns
+        -------
+        dist : np.ndarray
+            Each entry gives the list of distances to the neighbors of the
+            corresponding point.
+        """
+        # execute KNN query
+        if tree is None:
+            tree = cKDTree(arr)
+
+        dist, ind = tree.query(arr, k=k)
+
+        logger.debug('KNN index nearest neighbors: \n{}'.format(ind))
+        logger.debug('KNN distance: \n{}'.format(dist))
+
+        return dist
+
+    def _cluster(self, array, min_samples, eps=None, tree=False):
+        """
+        Run DBSCAN on array, compute eps if not supplied.
+
+        Parameters
+        ----------
+        array : ndarray
+            Array to be used for clustering, shape n samples with m features
+        min_samples : int
+            min_samples value for running DBSCAN
+        eps : float
+            Epsilon value for running DBSCAN
+            If None estimate using k-n distance and min_samples
+        tree : bool
+            Compute tree, if False use pre-computed tree
+
+        Returns
+        ---------
+        labels : ndarray
+            Vector of cluster labels for each row in array
+        eps : float
+            eps value used to run DBSCAN
+        min_samples : int
+            min_samples value used to run DBSCAN
+        """
+        if eps is None:
+            eps = self.optimal_eps(array, k=min_samples)
+
+        labels = DBSCAN(eps=eps, min_samples=min_samples).fit_predict(array)
+
+        n_dat = len(array)
+        if min_samples > n_dat:
+            raise ValueError('Number of nearest neighbors to retrieve '
+                             '(min_samples={}) exceeds length of the '
+                             'dataframe {}'.format(min_samples, n_dat))
+
+        if eps is None:
+            eps = self.optimal_eps(array, k=min_samples)
+
+        logger.debug('Set contains {} coordinates, '
+                     'N-neighbors required: {} at {}'
+                     .format(n_dat, min_samples, eps))
+        # get nearest neighbors for min_samples + 1 as there is self-reference
+        if tree:
+            tree = None
+        else:
+            tree = self._tree
+
+        d, _ = self.knn(array, tree=tree, k=min_samples + 1)
+        # count the number of distance results per index less than the
+        # threshold (exclude the 1st dist row which is self-referential)
+        counts = np.sum(d[:, 1:] <= eps, axis=1)
+
+        # find where there are NN neighbors satisfying the distance threshold
+        mask = counts >= min_samples
+        labels = np.zeros(len(array))
+        labels[mask] = 1
+
+        return labels, eps, min_samples
