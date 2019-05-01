06525c7558a1e48c76c7c203b683cac69b8d0ddc
Michael Rossol <michael.rossol@nrel.gov>
2019-05-01 08:19:17 -0600

    ct_to_cc refactor to restrict fillna to load

--- NaTGenPD/clean.py
+++ NaTGenPD/clean.py
@@ -707,20 +707,25 @@ class CleanSmoke:
         cc_unit : pandas.DataFrame
             CC unit data after aggregation of CT data
         """
+        cc_unit = cc_df.groupby('time')[['load', 'HTINPUT']].sum()
+        cc_unit = cc_unit.reset_index()
+        cc_unit['heat_rate'] = cc_unit['HTINPUT'] / cc_unit['load']
         try:
             unit_id = cc_df.name
         except AttributeError:
             unit_id = cc_df.iloc[0]['cc_unit']
 
-        cc_df.loc[cc_df['load'] <= 0, 'load'] = None
-        cc_df = cc_df.fillna(0)
-        group = cc_df.groupby('time')
-        cc_unit = group[['load', 'HTINPUT']].sum()
-        cc_unit = cc_unit.reset_index()
-        cc_unit['heat_rate'] = cc_unit['HTINPUT'] / cc_unit['load']
-        cts = group.apply(lambda x: len(x['load'].to_numpy().nonzero()[0]))
-        cc_unit['cts'] = cts.values
         cc_unit['unit_id'] = unit_id
+
+        cts_df = cc_df[['time', 'load']].copy()
+        cts_df.loc[cts_df['load'] <= 0, 'load'] = None
+
+        def get_cts(x):
+            cts = x['load'].fillna(0)
+            return len(cts.to_numpy().nonzero()[0])
+
+        cts = cts_df.groupby('time').apply(get_cts)
+        cc_unit['cts'] = cts.values
 
         info_cols = ['latitude', 'longitude', 'state', 'EPA_region',
                      'NERC_region', 'unit_type', 'fuel_type', 'group_type']
