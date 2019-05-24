73df12b0d6745ea0681bdf6f98d88e6f8e0c5b34
Michael Rossol <michael.rossol@nrel.gov>
2019-05-24 14:17:11 -0600

    update __init__.py

--- NaTGenPD/__init__.py
+++ NaTGenPD/__init__.py
@@ -5,6 +5,7 @@ The National Thermal Generator Performance Database.
 from .cli import setup_logger
 from .handler import CEMS
 from .clean import ParseSmoke, CleanSmoke
+from .filter import Filter, PolyFit, min_hr_filter
 
 __author__ = """Michael Rossol"""
 __email__ = "michael.rossol@nrel.gov"
