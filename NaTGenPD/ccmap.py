c357d2c1275ebb8181564a6cb8585dae7460fdb7
Michael Rossol <michael.rossol@nrel.gov>
2019-06-04 09:53:33 -0600

    cleanup ccmap

--- NaTGenPD/ccmap.py
+++ NaTGenPD/ccmap.py
@@ -1,23 +1,38 @@
+"""
+Map CEMS CC generators to EIA CC units
+"""
+import logging
 import numpy as np
 import pandas as pd
-import re
+
+logger = logging.getLogger(__name__)
+
 
 def method_1(boilers, eia_plants):
-
+    """
+    Method 1 to map boilers to eia plants
+    """
     # Create boiler-specific unit (Method 1)
-    no_eia_plant = boilers.loc[~np.in1d(boilers["Plant Code"], eia_plants), :].reset_index()
+    no_eia_plant = boilers.loc[~np.in1d(boilers["Plant Code"], eia_plants), :]
+    no_eia_plant = no_eia_plant.reset_index()
     no_eia_plant["Unit Code"] = no_eia_plant["Boiler ID"]
     no_eia_plant["Unit Code Method"] = 1
 
     return no_eia_plant
 
-def method_2_3(boilers23, boilers_generators, generators):
 
+def method_2_3(boilers23, boilers_generators, generators):
+    """
+    Method 2 and 3
+    """
     # Get boiler -> generator matches (Methods 2 + 3)
-    boilers_units = boilers23 \
-             .join(boilers_generators, on=["Plant Code", "Boiler ID"], how="inner") \
-             .join(generators[["Unit Code"]], on=["Plant Code", "Generator ID"], how="inner") \
-             .reset_index().drop_duplicates(["CEMSUnit", "Unit Code"])
+    boilers_units = boilers23.join(boilers_generators,
+                                   on=["Plant Code", "Boiler ID"], how="inner")
+    boilers_units = boilers_units.join(generators[["Unit Code"]],
+                                       on=["Plant Code", "Generator ID"],
+                                       how="inner")
+    boilers_units = boilers_units.reset_index().drop_duplicates(["CEMSUnit",
+                                                                "Unit Code"])
 
     gen_missing_unit_code = boilers_units["Unit Code"].isna()
 
@@ -34,10 +49,12 @@ def method_2_3(boilers23, boilers_generators, generators):
 
 
 def method_4(boilers4567, generators_cc):
-
+    """
+    Method 4
+    """
     # Check for no CA/CTs
-    boilers_plants = boilers4567.loc[
-        ~np.in1d(boilers4567["Plant Code"], generators_cc["Plant Code"]), :].copy()
+    boilers_plants = boilers4567.loc[~np.in1d(boilers4567["Plant Code"],
+                                     generators_cc["Plant Code"]), :].copy()
 
     # Create boiler-specific unit (Method 4)
     boilers_plants["Unit Code"] = boilers_plants["Boiler ID"].astype(str)
@@ -45,31 +62,42 @@ def method_4(boilers4567, generators_cc):
 
     return boilers_plants.reset_index()
 
+
 def method_5(boilers4567, generators_cc):
-
+    """
+    Method 5
+    """
     # Check for single unit code among all CA/CTs in plant
-    plants_units = generators_cc.loc[
-        np.in1d(generators_cc["Plant Code"], boilers4567["Plant Code"]),
-        ["Plant Code", "Unit Code"]].drop_duplicates().set_index("Plant Code")["Unit Code"]
+    pos = np.in1d(generators_cc["Plant Code"], boilers4567["Plant Code"])
+    plants_units = generators_cc.loc[pos, ["Plant Code", "Unit Code"]]
+    plants_units = plants_units.drop_duplicates().set_index("Plant Code")
+    plants_units = plants_units["Unit Code"]
     unit_code_count = plants_units.groupby(level="Plant Code").nunique()
-    single_unit_plants = unit_code_count.loc[unit_code_count == 1].index.get_values()
+    pos = unit_code_count == 1
+    single_unit_plants = unit_code_count.loc[pos].index.get_values()
 
-    # Assign all boilers in plant to same unit code if single unit code exists (Method 5)
+    # Assign all boilers in plant to same unit code if single unit code exists
+    # (Method 5)
     single_unit_plants = plants_units.loc[single_unit_plants]
-    result = boilers4567.join(single_unit_plants, on="Plant Code", how="right").reset_index()
+    result = boilers4567.join(single_unit_plants, on="Plant Code",
+                              how="right").reset_index()
     result["Unit Code Method"] = 5
 
     return result
 
 
 def method_6_7(boilers4567, generators_cc):
-
+    """
+    Method 6 and 7
+    """
     # Check for nonsingle unit code among all CA/CTs in plant
-    plants_units = generators_cc.loc[
-        np.in1d(generators_cc["Plant Code"], boilers4567["Plant Code"]),
-        ["Plant Code", "Unit Code"]].drop_duplicates().set_index("Plant Code")["Unit Code"]
+    pos = np.in1d(generators_cc["Plant Code"], boilers4567["Plant Code"])
+    plants_units = generators_cc.loc[pos, ["Plant Code", "Unit Code"]]
+    plants_units = plants_units.drop_duplicates().set_index("Plant Code")
+    plants_units = plants_units["Unit Code"]
     unit_code_count = plants_units.groupby(level="Plant Code").nunique()
-    nonsingle_unit_plants = unit_code_count.loc[unit_code_count != 1].index.get_values()
+    pos = unit_code_count != 1
+    nonsingle_unit_plants = unit_code_count.loc[pos].index.get_values()
 
     # Group boilers and generators by plant
     boiler_groups = boilers4567.loc[
@@ -92,7 +120,7 @@ def method_6_7(boilers4567, generators_cc):
         n_gs = len(gs.index)
 
         # Match boilers to generator unit codes (Method 6)
-        if n_bs <= n_gs: 
+        if n_bs <= n_gs:
             gs = gs.head(n_bs)
             result6 = result6.append(pd.DataFrame({
                 "CEMSUnit": np.array(bs["CEMSUnit"]),
@@ -106,17 +134,17 @@ def method_6_7(boilers4567, generators_cc):
         else:
             bs_rem = bs.tail(n_bs - n_gs)
             bs = bs.head(n_gs)
-            result7 = result7.append(pd.DataFrame({
-                "CEMSUnit": np.array(bs["CEMSUnit"]),
-                "Plant Code": plant,
-                "Boiler ID": np.array(bs["Boiler ID"]),
-                "Generator ID": np.array(gs["Generator ID"]),
-                "Unit Code": np.array(gs["Unit Code"])}), sort=True
-                ).append(pd.DataFrame({
-                "CEMSUnit": np.array(bs_rem["CEMSUnit"]),
-                "Plant Code": plant,
-                "Boiler ID": np.array(bs_rem["Boiler ID"]),
-                "Unit Code": np.array(bs_rem["Boiler ID"])}), sort=True)
+            df = pd.DataFrame({"CEMSUnit": np.array(bs["CEMSUnit"]),
+                               "Plant Code": plant,
+                               "Boiler ID": np.array(bs["Boiler ID"]),
+                               "Generator ID": np.array(gs["Generator ID"]),
+                               "Unit Code": np.array(gs["Unit Code"])})
+            result7 = result7.append(df, sort=True)
+            df = pd.DataFrame({"CEMSUnit": np.array(bs_rem["CEMSUnit"]),
+                               "Plant Code": plant,
+                               "Boiler ID": np.array(bs_rem["Boiler ID"]),
+                               "Unit Code": np.array(bs_rem["Boiler ID"])})
+            result7 = result7.append(df, sort=True)
 
     result6["Unit Code Method"] = 6
     result7["Unit Code Method"] = 7
@@ -128,21 +156,27 @@ if __name__ == "__main__":
 
     # Load CEMS boilers
     boilers = pd.read_csv("../bin/emission_01-17-2017.csv",
-        usecols=[2,3,25], header=0, names=["Plant Code", "Boiler ID", "Unit Type"])
-    boilers = boilers.loc[["combined cycle" in ut.lower() for ut in boilers["Unit Type"]] , :]
+                          usecols=[2, 3, 25], header=0,
+                          names=["Plant Code", "Boiler ID", "Unit Type"])
+    boilers = boilers.loc[["combined cycle" in ut.lower()
+                           for ut in boilers["Unit Type"]], :]
     boilers.drop("Unit Type", axis=1, inplace=True)
-    boilers.index = boilers["Plant Code"].astype(str) + "_" + boilers["Boiler ID"]
+    index = boilers["Plant Code"].astype(str) + "_" + boilers["Boiler ID"]
+    boilers.index = index
     boilers.index.name = "CEMSUnit"
 
     # Load boiler-generator mapping
     boilers_generators = pd.read_excel(
         "../bin/6_1_EnviroAssoc_Y2017.xlsx", "Boiler Generator",
-        header=1, usecols=[2,4,5],
-        index_col=[0,1], skipfooter=1)
+        header=1, usecols=[2, 4, 5],
+        index_col=[0, 1], skipfooter=1)
 
     def read_generators(f, sheet):
-        return f.parse(sheet, header=1, usecols=[2,6,8,9],
-                       index_col=[0,1], skipfooter=1)
+        """
+        Read generator from excel sheet
+        """
+        return f.parse(sheet, header=1, usecols=[2, 6, 8, 9],
+                       index_col=[0, 1], skipfooter=1)
 
     # Load generator-unit mapping
     with pd.ExcelFile("../bin/3_1_Generator_Y2017.xlsx") as f:
@@ -150,8 +184,10 @@ if __name__ == "__main__":
         generators_retired = read_generators(f, "Retired and Canceled")
         generators_proposed = read_generators(f, "Proposed")
 
-    generators = generators.append(generators_retired, sort=True).append(generators_proposed, sort=True)
-    generators_cc = generators.loc[np.in1d(generators["Prime Mover"], ["CA", "CT"]), :].reset_index()
+    generators = generators.append(generators_retired, sort=True)
+    generators = generators.append(generators_proposed, sort=True)
+    pos = np.in1d(generators["Prime Mover"], ["CA", "CT"])
+    generators_cc = generators.loc[pos, :].reset_index()
 
     # Any CC CA/CTs without a unit code are assigned to a plant-wide unit
     gcc_nounitcode = generators_cc["Unit Code"].isna()
@@ -167,21 +203,25 @@ if __name__ == "__main__":
     boilers_23 = boilers_234567.loc[boilers_23, :]
 
     # Process CEMS boiler + EIA860 plant/generator data
-    result1          = method_1(boilers, eia_plants)
+    result1 = method_1(boilers, eia_plants)
     result2, result3 = method_2_3(boilers_23, boilers_generators, generators)
-    result4          = method_4(boilers_4567, generators_cc)
-    result5          = method_5(boilers_4567, generators_cc)
+    result4 = method_4(boilers_4567, generators_cc)
+    result5 = method_5(boilers_4567, generators_cc)
     result6, result7 = method_6_7(boilers_4567, generators_cc)
 
     # Recombine results from all methods
-    result = pd.concat([result1, result2, result3, result4,
-                        result5, result6, result7], sort=True).set_index("CEMSUnit")[
-                        ["Plant Code", "Boiler ID", "Generator ID", "Unit Code", "Unit Code Method"]]
-    result["CCUnit"] = result["Plant Code"].astype(str) + "_" + result["Unit Code"].astype(str)
+    result = pd.concat([result1, result2, result3, result4, result5, result6,
+                        result7], sort=True).set_index("CEMSUnit")
+    results = results[["Plant Code", "Boiler ID", "Generator ID", "Unit Code",
+                       "Unit Code Method"]]
+    plant_code = result["Plant Code"].astype(str)
+    unit_code = result["Unit Code"].astype(str)
+    result["CCUnit"] = plant_code + "_" + unit_code
 
-    print(str(len(boilers.index)) + " input boilers")
-    print(str(len(result.index)) + " output mappings")
-    print(str(len(result.drop_duplicates(["Plant Code", "Boiler ID"]).index)) + " unique output mappings")
-    print(result.groupby("Unit Code Method").count())
+    logger.info("{} input boilers".format(len(boilers.index)))
+    logger.info("{} output mappings".format(len(result.index)))
+    outputs = len(result.drop_duplicates(["Plant Code", "Boiler ID"]).index)
+    logger.info("{} unique output mappings".format(outputs))
+    logger.info(result.groupby("Unit Code Method").count())
 
     result.to_csv("../bin/cems_cc_mapping.csv")
